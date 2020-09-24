35c8a52d52ef422a9cf8e32bb0ee4485b5643ecf
Takkuya <takuyagabriel@gmail.com>
2020-09-24 02:11:47 -0300

    Implementação da navegação do header, tema escuro e footer. Projeto finalizado

---
+++ youtube-clone-github/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Line, GithubLogo } from "./styles";
+
+export default function Footer() {
+  return (
+    <Container>
+      <Line />
+      <GithubLogo />
+    </Container>
+  );
+}
--- youtube-clone-github/src/components/Header/styles.ts
+++ youtube-clone-github/src/components/Header/styles.ts
@@ -1,35 +1,42 @@
-import styled from 'styled-components';
-import { FaGithub } from 'react-icons/fa'
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    background: var(--header);
-    padding: 11px 16px;
+  display: flex;
+  align-items: center;
+  background: var(--header);
+  padding: 11px 16px;
 `;
 
 export const GitHubLogo = styled(FaGithub)`
-    fill: var(--logo);
-    width: 32px;
-    height: 32px;
-    flex-shrink: 0; /*A logo não é "amassada" quando diminuimos a responsibilidade*/
-`
+  fill: var(--logo);
+  width: 32px;
+  height: 32px;
+  flex-shrink: 0; /*A logo não é "amassada" quando diminuimos a responsibilidade*/
+
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 export const SearchForm = styled.form`
-    padding-left: 16px;
+  padding-left: 16px;
+  width: 100%;
+
+  input {
+    background: var(--search);
+    outline: 0; /*Tira aquela linha branca em volta dos campos do form*/
+    border-radius: 6px;
+    padding: 7px 12px;
     width: 100%;
+    color: var(--primary);
 
-    input {
-        background: var(--search);
-        outline: 0; /*Tira aquela linha branca em volta dos campos do form*/
-        border-radius: 6px;
-        padding: 7px 12px;
-        width: 100%;
-
-        &:focus { /*Para quando clicarem no input ele executar tal ação, nesse caso vai ser aumentar a barrinha e mudar ela de cor*/
-            width: 318px;
-        }
- 
-        /* Responsável pela animação que a barra faz*/
-        transition: width 0.2s ease-out, color 0.2s ease-out;
+    &:focus {
+      /*Para quando clicarem no input ele executar tal ação, nesse caso vai ser aumentar a barrinha e mudar ela de cor*/
+      width: 318px;
     }
-`
+
+    /* Responsável pela animação que a barra faz*/
+    transition: width 0.2s ease-out, color 0.2s ease-out;
+  }
+`;
